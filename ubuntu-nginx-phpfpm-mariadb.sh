79a4f6202e56c7c19724aa3abfddd34eebe3755c
Ish Sookun <ish@hacklog.mu>
2017-03-07 00:13:13 +0400

    edit nginx config

--- ubuntu-nginx-phpfpm-mariadb.sh
+++ ubuntu-nginx-phpfpm-mariadb.sh
@@ -11,9 +11,9 @@ create-ds-network() {
 	docker network create --driver bridge --subnet 172.200.0.0/16 lexp_net > /dev/null 2>&1
 }
 
-create-lexp-web() {
-	docker run --rm --net lexp_net --name lexp_web \
-		-v ${PROJECT_PATH}/config/lexp/nginx:/etc/nginx \
+create-ds-nginx() {
+	docker run --rm --net ds_net --name ds_nginx \
+		-v ${PROJECT_PATH}/config/nginx:/etc/nginx \
 		-v ${PROJECT_PATH}/www:/var/www nginx > /dev/null 2>&1 &
 		sleep 2
 }
